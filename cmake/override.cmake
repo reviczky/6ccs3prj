6c875515054caa24e385112a1bb76bcc48eb662a
Adam REVICZKY <adam.reviczky@kcl.ac.uk>
2009-02-25 22:17:07 +0000

    update override cmake syntax

--- cmake/override.cmake
+++ cmake/override.cmake
@@ -1,11 +1,13 @@
-# compile a Java file into an object file
-FILE(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/.class")
+# compiling Java source files into object files
+FILE (MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/.class")
 
-SET(CMAKE_Java_COMPILE_OBJECT
-  "<CMAKE_Java_COMPILER> <FLAGS> -encoding utf8 <SOURCE> -d \"<CMAKE_CURRENT_BINARY_DIR>/.class\""
-)
+# default charset
+SET (ENCODING utf8)
 
-# this is a place holder if java needed flags for javac they would go here.
-SET(CMAKE_Java_CREATE_STATIC_LIBRARY
-  "<CMAKE_Java_ARCHIVE> -cfm <TARGET> Manifest.txt -C \"<CMAKE_CURRENT_BINARY_DIR>/.class\" ."
-)
+SET (CMAKE_Java_COMPILE_OBJECT "<CMAKE_Java_COMPILER> <FLAGS> -encoding ${ENCODING} <SOURCE> -d \"<CMAKE_CURRENT_BINARY_DIR>/.class\"")
+
+# default Manifest
+SET (MANIFEST Manifest.txt)
+
+# Java compiler flags
+SET (CMAKE_Java_CREATE_STATIC_LIBRARY "<CMAKE_Java_ARCHIVE> -cfm <TARGET> ${MANIFEST} -C \"<CMAKE_CURRENT_BINARY_DIR>/.class\" .")
